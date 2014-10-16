c3e12a92a2e95ce7f64fb27f9c53f848af17b87f
ks7000 <jimmyolano@hotmail.com>
2014-10-16 15:44:03 -0430

    Instrucciones para depurar con pdb

---
+++ archivos.py
@@ -0,0 +1,67 @@
+# -*- encoding=utf-8 -*-
+import os
+import sys
+import glob
+import pdb
+pdb.set_trace()
+def lee(*args):
+    for i in args:
+        if isinstance(i,str):
+            #if os.access(i, os.R_OK): 
+            if existe_archivo(i):
+                arch = raw_input("Introduzca el nombre del archivo a copiar:");
+                if arch<> "":
+                    copiar = False                    
+                    if os.path.exists(arch):
+                        resp = raw_input("El archivo existe, ¿Desea sobreescribirlo? [s/n]");
+                        if resp == "s" or resp == "S":
+                            copiar = True
+                    else:
+                        copiar = True
+                    if copiar:                    
+                        f = open(i)
+                        g = open(arch, "w")
+                        g.write("Este archivo fue copiado de ")
+                        g.write(os.path.realpath(i))
+                        for linea in f:
+                            print linea
+                            g.write(linea)
+                        f.close()
+                        g.close()
+                        lee_archivo_varias_lineas(i)
+            else:
+                print "El archivo no existe."
+
+def existe_archivo(archivo):
+    if os.path.exists(archivo):
+        return True
+    else:
+        resp = raw_input("El archivo NO existe, ¿Desea crearlo? [s/n]");
+        if resp == "s" or resp == "S":
+            if sys.platform == "linux2":
+                os.system('touch ' + archivo )
+            return True
+        else:
+            return False
+
+def lee_archivo_varias_lineas(archivo):
+    f=open(archivo)
+    print "--------------------------------"
+    print "Lee varias lineas sin ciclo for:"
+    print (f.readlines())
+    f.close
+
+#def lista_txt():
+#    nombres = os.listdir(os.getcwd))
+#    for i in nombres:
+#        cad = i
+#        if string.find(cad,"*.txt"):
+#            print cad
+        
+            
+if sys.platform == "linux2":
+    os.system('clear')
+#lista_txt()
+print glob.glob('*.txt')
+archivo = raw_input("Introduzca el(los) nombre(s) de el(os) archivo(s) original(es):");
+lee(archivo)
